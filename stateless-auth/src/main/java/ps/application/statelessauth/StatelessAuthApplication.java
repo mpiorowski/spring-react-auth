e234e738bbfd0501365df3c3790dc8a7bdcc784c
Mateusz Piórowski <mateuszp@lsnova.pl>
2018-09-25 10:07:26 +0200

    add stateless-auth

---
+++ stateless-auth/src/main/java/ps/application/statelessauth/StatelessAuthApplication.java
@@ -0,0 +1,12 @@
+package ps.application.statelessauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatelessAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatelessAuthApplication.class, args);
+	}
+}
---
+++ stateless-auth/src/test/java/ps/application/statelessauth/StatelessAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package ps.application.statelessauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatelessAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
