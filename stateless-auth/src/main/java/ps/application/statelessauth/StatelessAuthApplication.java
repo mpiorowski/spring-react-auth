b2dd5796100c04adb940a16f15ddb124bd384a5d
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-09-28 11:48:03 +0200

    test

--- stateless-auth/src/main/java/ps/application/statelessauth/StatelessAuthApplication.java
+++ stateless-auth/src/main/java/ps/application/statelessauth/StatelessAuthApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StatelessAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StatelessAuthApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StatelessAuthApplication.class, args);
+  }
+
+  // test
 }
