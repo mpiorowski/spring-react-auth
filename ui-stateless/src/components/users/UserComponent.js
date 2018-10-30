71db176d7815ef7f62b241cce1aa7654bce98353
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-10-30 13:23:51 +0100

    working table with users

--- ui-stateless/src/components/users/UserComponent.js
+++ ui-stateless/src/components/users/UserComponent.js
@@ -1,29 +1,56 @@
 import React, {Component} from 'react';
 import {Col, Row, Table} from 'antd';
 import "./UserComponent.css";
+import {getAllUsers} from "../../service/UserService";
 
+let uuid = 0;
+let keys = [];
 class UserComponent extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableLoading: true,
+      tableData: [],
+    }
+  }
 
   componentWillMount() {
-
+    getAllUsers().then(response => {
+      if (response) {
+        this.add(response);
+      }
+    });
   }
+
+  add = (val) => {
+    const tableData = val.map(val => {
+      return {username: val.username, password: val.password};
+    });
+    this.setState({
+      tableData: tableData,
+      tableLoading: false,
+    })
+
+  };
 
 
   render() {
 
-    const columns = [{
-      title: 'Name',
-      dataIndex: 'name',
-    }, {
-      title: 'age',
-      dataIndex: 'age',
+    const dataSource = [{
+      key: '1',
+      name: 'Mike',
+      password: 32,
     }];
 
-    const data = [{
-      key: '1',
-      name: 'mat',
-      age: '22'
+    console.log(dataSource);
+
+    const columns = [{
+      title: 'Username',
+      dataIndex: 'username',
+    }, {
+      title: 'Password',
+      dataIndex: 'password',
     }];
 
     function onChange() {
@@ -34,7 +61,7 @@ class UserComponent extends Component {
     return (
         <Row type="flex" justify="center" align="top" style={{width: '100%'}}>
           <Col span={18}>
-            <Table columns={columns} dataSource={data} onChange={onChange} className={"userTable"} size="middle"/>
+            <Table columns={columns} dataSource={this.state.tableData} onChange={onChange} loading={this.state.tableLoading} className={"userTable"} size="middle"/>
           </Col>
         </Row>
     );
