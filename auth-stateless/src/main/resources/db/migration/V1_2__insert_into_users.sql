bc4b0553b8ebdee1a48615244cca30c313e6209a
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-11-09 14:06:51 +0100

    finished docker + renamed app

--- auth-stateless/src/main/resources/db/migration/V1_2__insert_into_users.sql
+++
@@ -1,4 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-INSERT into users (user_name, user_mail, user_password, user_role) VALUES ('mat', 'mat@gmail.com', crypt('pass', gen_salt('bf', 8)), 'admin');
-INSERT into users (user_name, user_mail, user_password, user_role) VALUES ('patryk', 'patryk@gmail.com',crypt('pass', gen_salt('bf', 8)), 'admin');
-INSERT into users (user_name, user_mail, user_password, user_role) VALUES ('user', 'user@gmail.com',crypt('pass', gen_salt('bf', 8)), 'user');
--- ui-stateless/src/config/config.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE_URL = 'http://localhost:9000';
-export const ACCESS_TOKEN = 'jwtToken';
