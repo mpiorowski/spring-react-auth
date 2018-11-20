cadfb716bc67f5ab94a16d1ca98129a75e0b3b64
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-11-20 09:25:25 +0100

    swagger

--- api/src/main/java/auth/api/StatelessAuthApplication.java
+++ api/src/main/java/auth/api/StatelessAuthApplication.java
@@ -2,7 +2,6 @@ package auth.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class StatelessAuthApplication {
