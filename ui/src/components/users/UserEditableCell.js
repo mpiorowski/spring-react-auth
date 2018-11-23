84db16c4bcc0beb98254120cf648cc889926d900
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-11-23 11:12:43 +0100

    EditableContext

---
+++ ui/src/components/users/UserEditableCell.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {Form, Input, InputNumber} from 'antd';
+
+const FormItem = Form.Item;
+export const EditableContext = React.createContext();
+
+const EditableRow = ({form, index, ...props}) => (
+    <EditableContext.Provider value={form}>
+      <tr {...props} />
+    </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class UserEditableCell extends Component {
+
+  getInput = () => {
+    if (this.props.inputType === 'number') {
+      return <InputNumber/>;
+    }
+    return <Input/>;
+  };
+
+
+  render() {
+
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      ...restProps
+    } = this.props;
+
+    return (
+        <div>
+          <EditableContext.Consumer>
+            {(form) => {
+              const {getFieldDecorator} = form;
+              return (
+                  <td {...restProps}>
+                    {editing ? (
+                        <FormItem style={{margin: 0}}>
+                          {getFieldDecorator(dataIndex, {
+                            rules: [{
+                              required: true,
+                              message: `Please Input ${title} !`,
+                            }],
+                            initialValue: record[dataIndex],
+                          })(this.getInput())}
+                        </FormItem>
+                    ) : restProps.children}
+                  </td>
+              );
+            }}
+          </EditableContext.Consumer>
+        </div>);
+  }
+}
+
+export default UserEditableCell;
