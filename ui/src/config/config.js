175e5c83584884518dc8847646d692ef1bd21d47
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-11-29 11:44:59 +0100

    database files seperated for dev and prod, same for ports

--- ui/src/config/config.js
+++ ui/src/config/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = 'http://localhost:9000';
+export const PROFILE = process.env.REACT_APP_STAGE === 'prod' ? 'prod' : 'dev';
+export const API_BASE_URL = PROFILE === 'dev' ? 'http://localhost:9000' : "http://localhost:9001";
 export const ACCESS_TOKEN = 'jwtToken';
