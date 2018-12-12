a0094bdd691e7aae9d2108fef60447b79840cdc0
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-12-12 13:34:31 +0100

    config

--- ui/src/config/config.js
+++ ui/src/config/config.js
@@ -1,3 +1,3 @@
 export const PROFILE = process.env.REACT_APP_STAGE === 'prod' ? 'prod' : 'dev';
-export const API_BASE_URL = PROFILE === 'dev' ? 'http://localhost:9000' : "http://localhost:9001";
+export const API_BASE_URL = PROFILE === 'dev' ? 'http://localhost:9000' : "http://ec2-18-184-206-176.eu-central-1.compute.amazonaws.com:9001";
 export const ACCESS_TOKEN = 'jwtToken';
